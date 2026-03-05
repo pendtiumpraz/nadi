c6d24c2cc0dbe576d0d4b4c9b78fd4181b712f5e
Galih Praz <pendtiumpraz@gmail.com>
2026-03-05 09:27:33 +0700

    fix: use relative imports in LandingSwitcher

--- src/components/LandingSwitcher.tsx
+++ src/components/LandingSwitcher.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const NewLanding = dynamic(() => import("@/components/NewLanding"), { ssr: false });
-const OldLanding = dynamic(() => import("@/components/OldLanding"), { ssr: false });
-const MaintenanceLanding = dynamic(() => import("@/components/MaintenanceLanding"), { ssr: false });
+import NewLanding from "./NewLanding";
+import OldLanding from "./OldLanding";
+import MaintenanceLanding from "./MaintenanceLanding";
 
 export default function LandingSwitcher({ version }: { version: string }) {
     if (version === "maintenance") return <MaintenanceLanding />;
